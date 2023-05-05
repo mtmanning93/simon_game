a9da8e3b644d13372c4779f5c15e1afaa5098d2c
Matt Manning <mtmanning93@hotmail.co.uk>
2023-05-05 19:51:35 +0000

    Finish boilerplate set up

---
+++ scripts/tests/game.test.js
@@ -0,0 +1,10 @@
+/**
+ * @jest-environment jsdom
+ */
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
